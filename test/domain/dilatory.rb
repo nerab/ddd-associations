beaec3049d3f611f4e6bf5c5c7181bd66a06b377
Nicholas E. Rabenau <nerab@gmx.net>
2012-07-16 23:38:59 +0200

    Remove domain-specific collection class; it was replaced with has_many

--- test/domain/dilatory.rb
+++ test/domain/dilatory.rb
@@ -4,6 +4,5 @@ require 'domain/dilatory/task'
 require 'domain/dilatory/project'
 require 'domain/dilatory/tag'
 
-require 'domain/dilatory/project_tasks'
 require 'domain/dilatory/tag_tasks'
 require 'domain/dilatory/task_tags'
