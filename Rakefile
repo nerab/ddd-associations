d9588733b1d2a56f7e25bf47c0cefe0f0f12dc7f
Nicholas E. Rabenau <nerab@gmx.net>
2012-07-17 21:37:38 +0200

    Add README to sample app

--- Rakefile
+++ Rakefile
@@ -1,5 +1,6 @@
 #!/usr/bin/env rake
 
+require 'bundler/gem_tasks'
 require 'rake/testtask'
 
 Rake::TestTask.new do |test|
--- lib/ddd-associations.rb
+++ lib/ddd-associations.rb
@@ -3,6 +3,5 @@ require 'forwardable'
 require 'active_support/inflector'
 
 require 'ddd-associations/associations'
-
 require 'ddd-associations/one_to_many_collection'
 require 'ddd-associations/many_to_many_collection'
