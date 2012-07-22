0ee37bbd60cb78c1ad43ba468dcbec4f2b28b329
Nicholas E. Rabenau <nerab@gmx.net>
2012-07-22 10:11:30 +0200

    Accidential edit

---
+++ test/unit/test_task_repository.rb
@@ -0,0 +1,24 @@
+require 'helper'
+require 'tempfile'
+
+class TestTaskRepository < MiniTest::Unit::TestCase
+  include Dilatory
+
+  def setup
+    @data_dir = Dir.mktmpdir
+    @repo = Dilatory::TaskRepository.new(@data_dir)
+  end
+
+  def teardown
+    FileUtils.rm_r(@data_dir) if @data_dir && Dir.exist?(@data_dir)
+  end
+
+  def test_all_empty
+    assert_equal(0, @repo.all.size)
+  end
+
+  def test_save_task
+    @repo.save(Task.new('Read DDD book'))
+    assert_equal(1, @repo.all.size)
+  end
+end
